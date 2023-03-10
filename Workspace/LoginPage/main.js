092f96b371099b5b14ca589f22081a2b11b050f6
ZivFerdinand <71594716+ZivFerdinand@users.noreply.github.com>
2023-03-10 12:29:38 +0700

    Update LogIn

--- Workspace/LoginPage/main.js
+++ Workspace/LoginPage/main.js
@@ -13,6 +13,45 @@ const divTermsError = document.getElementById("termsCheckboxLabel")
 const incrBtn = document.getElementById("up-btn");
 const decrBtn = document.getElementById("down-btn");
 
+
+let selector = document.getElementById("selector");
+let home = document.getElementById("home");
+let char = document.getElementById("char");
+let maps = document.getElementById("maps");
+let abts = document.getElementById("abts");
+
+function resetPos()
+{
+	selector.style.marginLeft = "0%";
+	selector.style.width = "0%";
+}
+selector.style.transition = "all .5s";
+home.addEventListener("mouseenter", () => {
+	selector.style.marginLeft = "8.5%";
+	selector.style.width = "12%";
+});
+home.addEventListener("mouseleave", resetPos);
+
+char.addEventListener("mouseenter", () => {
+    console.log('XX')
+	selector.style.marginLeft = "30%";
+	selector.style.width = "24%";
+});
+char.addEventListener("mouseleave", resetPos);
+
+maps.addEventListener("mouseenter", () => {
+	selector.style.marginLeft = "64.3%";
+	selector.style.width = "10%";
+});
+maps.addEventListener("mouseleave", resetPos);
+
+abts.addEventListener("mouseenter", () => {
+	selector.style.marginLeft = "84%";
+	selector.style.width = "15%";
+});
+abts.addEventListener("mouseleave", resetPos);
+
+
 incrBtn.addEventListener("click", () => {
     if (inputAge.value == "" || inputAge.value < 14)
     {
