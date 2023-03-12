ea7ce8b549988b1de558d8296299d6c9720e75c2
ZivFerdinand <71594716+ZivFerdinand@users.noreply.github.com>
2023-03-12 12:57:18 +0700

    Updated Home Screen

--- Workspace/MapScreen/main.js
+++ Workspace/MapScreen/main.js
@@ -34,42 +34,28 @@ abts.addEventListener("mouseenter", () => {
 });
 abts.addEventListener("mouseleave", resetPos);
 
-document.getElementById("overlayMyramid").style.transition = ".5s"
-async function on() {
-	document.getElementById("overlayMyramid").style.opacity = "0";
-	document.getElementById("overlayMyramid").style.display = "flex";
-	await delay(500)
-	
-	document.getElementById("overlayMyramid").style.opacity = "1";
-}
 
-async function off() {
-	document.getElementById("overlayMyramid").style.opacity = "0";
-	await delay(500)
-	document.getElementById("overlayMyramid").style.display = "none";
-}
-
-var imagesMyramid = new Array();
+var imagesHero = new Array();
 let x = 0
-imagesMyramid[0] = "url('Images/myramid1.svg')";
-imagesMyramid[1] = "url('Images/myramid2.svg')";
-imagesMyramid[2] = "url('Images/myramid3.svg')";
-imagesMyramid[3] = "url('Images/myramid4.svg')";
-imagesMyramid[4] = "url('Images/myramid5.svg')";
+imagesHero[0] = "url('Images/myramid1.svg')";
+imagesHero[1] = "url('Images/myramid2.svg')";
+imagesHero[2] = "url('Images/myramid3.svg')";
+imagesHero[3] = "url('Images/myramid4.svg')";
+imagesHero[4] = "url('Images/myramid5.svg')";
 
-var textMyramid = new Array();
-textMyramid[0] = "[1] Cider Lake"
-textMyramid[1] = "[2] Falcon Coast"
-textMyramid[2] = "[3] Cape Oath"
-textMyramid[3] = "[4] Dawn Winery"
-textMyramid[4] = "[5] Springvale"
+var textTitleHero = new Array();
+textTitleHero[0] = "[1] Cider Lake"
+textTitleHero[1] = "[2] Falcon Coast"
+textTitleHero[2] = "[3] Cape Oath"
+textTitleHero[3] = "[4] Dawn Winery"
+textTitleHero[4] = "[5] Springvale"
 
-var descMyramid = new Array();
-descMyramid[0] ="A natural freshwater lake. The lake's wide, crystalline appearance has remained unchanged for eons — so clear you can see the bottom, and cool and refreshing to drink. It is none other than these headwaters that brought Myramid's ciders and wines to fame and made the lake the symbol of Myramid that it is today.";
-descMyramid[1] ="Stretching eastward from Windrise to the sea, Falcon coast — together with Cape Oath — shelters the sea from three sides, creating a bay that sits east of Myramid. Given the long coastline and shallow waters, citizens of Myramid often come here to enjoy a nice stroll. On occasion, you can see falcons soaring high above the blue waves that brush upon the white sand."
-descMyramid[2] ="In the southeast corner of Myriad's surrounds lies a cape that borders with Galesong Hill. Legend has it two lovers made an oath to each other here and left behind a deeply touching story. To this day the area is still known to be popular amongst young lovebirds. Other than being a great vantage point for watching sunrises and sunsets, the drop in terrain makes it easy to see Windrise, it sits facing the sea."
-descMyramid[3] ="Nestled in the land southwest of Myramid sits the winery that has passed down the legacy of Myramid's traditional wines and ciders for generations. The grapes grown here are used to produce a range of wines that are popular right across Teyvat. Every year's products are split, with a portion being sold locally within Myramid, the rest is sold off via the trade route that sits south of the city."
-descMyramid[4] ="A sleepy little village to the south of Myramid, that sits facing the lake that surrounds the city. Most of the village's residents are hunters that provide fresh meat for all the big restaurants of Myramid. Springvale's rustic appeal attracts many tourists. If you are lucky in your travels, you may come across a restaurant that truly knows how to bring out the best qualities of Springvale's meat."
+var textDescHero = new Array();
+textDescHero[0] ="A natural freshwater lake. The lake's wide, crystalline appearance has remained unchanged for eons — so clear you can see the bottom, and cool and refreshing to drink. It is none other than these headwaters that brought Myramid's ciders and wines to fame and made the lake the symbol of Myramid that it is today.";
+textDescHero[1] ="Stretching eastward from Windrise to the sea, Falcon coast — together with Cape Oath — shelters the sea from three sides, creating a bay that sits east of Myramid. Given the long coastline and shallow waters, citizens of Myramid often come here to enjoy a nice stroll. On occasion, you can see falcons soaring high above the blue waves that brush upon the white sand."
+textDescHero[2] ="In the southeast corner of Myriad's surrounds lies a cape that borders with Galesong Hill. Legend has it two lovers made an oath to each other here and left behind a deeply touching story. To this day the area is still known to be popular amongst young lovebirds. Other than being a great vantage point for watching sunrises and sunsets, the drop in terrain makes it easy to see Windrise, it sits facing the sea."
+textDescHero[3] ="Nestled in the land southwest of Myramid sits the winery that has passed down the legacy of Myramid's traditional wines and ciders for generations. The grapes grown here are used to produce a range of wines that are popular right across Teyvat. Every year's products are split, with a portion being sold locally within Myramid, the rest is sold off via the trade route that sits south of the city."
+textDescHero[4] ="A sleepy little village to the south of Myramid, that sits facing the lake that surrounds the city. Most of the village's residents are hunters that provide fresh meat for all the big restaurants of Myramid. Springvale's rustic appeal attracts many tourists. If you are lucky in your travels, you may come across a restaurant that truly knows how to bring out the best qualities of Springvale's meat."
 
 
 let mainImgMyramid = document.getElementById("main-BG-Myramid");
@@ -103,4 +89,20 @@ async function changeImage(imgToChange, ttlToChange, descToChange, imgs, ttls, d
   changeImage(imgToChange, ttlToChange, descToChange, imgs, ttls, descs)
 }
 
-changeImage(mainImgMyramid, mainTlMyramid, mainDescMyramid, imagesMyramid, textMyramid, descMyramid)
+changeImage(mainImgMyramid, mainTlMyramid, mainDescMyramid, imagesHero, textTitleHero, textDescHero)
+
+
+document.getElementById("overlayMyramid").style.transition = ".5s";
+async function on() {
+	document.getElementById("overlayMyramid").style.opacity = "0";
+	document.getElementById("overlayMyramid").style.display = "flex";
+	await delay(500);
+
+	document.getElementById("overlayMyramid").style.opacity = "1";
+}
+
+async function off() {
+	document.getElementById("overlayMyramid").style.opacity = "0";
+	await delay(500);
+	document.getElementById("overlayMyramid").style.display = "none";
+}
